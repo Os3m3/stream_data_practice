0422ca6bd2c3bf4da47eb84ecb9e2ba87a5f2264
os3m3 <osama.zakwani@gmail.com>
2026-06-15 16:21:57 +0400

    Added the database and the clean data go to the database directly

--- database_consumer.py
+++ database_consumer.py
@@ -3,7 +3,9 @@ from logger import log
 import psycopg
 import os
 from dotenv import load_dotenv
+import json
 
+load_dotenv()  
 
 consumer = Consumer({
     "bootstrap.servers" : "localhost:9092",
@@ -11,7 +13,7 @@ consumer = Consumer({
     "auto.offset.reset" : "earliest"
 })
 
-consumer.subscribe(['clean-drillinf-data'])
+consumer.subscribe(['clean-drilling-data'])
 
 db_conn = psycopg.connect(
     host="localhost",
@@ -23,9 +25,12 @@ db_conn = psycopg.connect(
 
 cursor = db_conn.cursor()
 
-cursor.execute=(""" 
-CREATE TABLE IF NOT EXIST clean_drilling_data (
-   sequence_number BIGINT PRIMARY KEY,
+cursor.execute("DROP TABLE IF EXISTS clean_drilling_data CASCADE;")
+db_conn.commit()
+
+cursor.execute(""" 
+CREATE TABLE IF NOT EXISTS clean_drilling_data (
+    sequence_number BIGINT,
     well_uid TEXT,
     wellbore_uid TEXT,
     log_uid TEXT,
@@ -38,13 +43,14 @@ CREATE TABLE IF NOT EXIST clean_drilling_data (
     torque_ftlb DOUBLE PRECISION,
     standpipe_pressure_psi DOUBLE PRECISION,
     mud_flow_lpm DOUBLE PRECISION,
-    hookload_klbf DOUBLE PRECISION               
+    hookload_klbf DOUBLE PRECISION,
+    PRIMARY KEY (sequence_number, time)               
 );
 """)
 
 db_conn.commit()
 
-cursor.execute=("""
+cursor.execute("""
 SELECT create_hypertable(
   'clean_drilling_data',
   'time'
@@ -56,3 +62,76 @@ db_conn.commit()
 log.info("Database consumer started polling")
 log.info("clean_drilling_data table is ready")
 
+while True:
+    try:
+        msg = consumer.poll(1.0)
+
+        if msg is None:
+            continue
+
+        if msg.error():
+            print("Consumer error:", msg.error())
+            continue
+
+        msg_text = msg.value().decode("utf-8")
+        clean_data = json.loads(msg_text)
+
+        values = clean_data["data"]
+
+        timestamp = values[0]
+        md = values[1]
+        rop = values[2]
+        wob = values[3]
+        rpm = values[4]
+        torque = values[5]
+        spp = values[6]
+        flow = values[7]
+        hkld = values[8]
+
+        cursor.execute(
+            """
+            INSERT INTO clean_drilling_data (
+                sequence_number,
+                well_uid,
+                wellbore_uid,
+                log_uid,
+                rig_id,
+                time,
+                measured_depth_m,
+                rop_m_per_hr,
+                wob_klbf,
+                rpm,
+                torque_ftlb,
+                standpipe_pressure_psi,
+                mud_flow_lpm,
+                hookload_klbf
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (sequence_number, time) DO NOTHING;
+            """,
+            (
+                clean_data["sequence_number"],
+                clean_data["well_uid"],
+                clean_data["wellbore_uid"],
+                clean_data["log_uid"],
+                clean_data["rig_id"],
+                timestamp,
+                md,
+                rop,
+                wob,
+                rpm,
+                torque,
+                spp,
+                flow,
+                hkld
+            )
+        )
+
+        db_conn.commit()
+
+        log.info(f"Inserted clean record: {clean_data['sequence_number']}")
+        print("Inserted clean record:", clean_data["sequence_number"])
+
+    except Exception as e:
+        print("Error:", e)
+        db_conn.rollback()
